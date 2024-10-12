1755eb3766bf90e938fc6c15a178feb4872f2e68
Kaulina <iv_kaulina@mail.ru>
2024-10-12 21:03:36 +0700

    JPA_Repositories

--- src/main/java/ru/netology/entity/Person.java
+++ src/main/java/ru/netology/entity/Person.java
@@ -3,19 +3,21 @@ package ru.netology.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.*;
 
+@Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-@Entity
+@Table(schema = "migration", name = "person")
 public class Person {
     @EmbeddedId
     private DataPerson dataPerson;
-    @Column(nullable = false)
-    private String phone_number;
-    @Column(nullable = false, length = 200)
-    private String city_of_living;
+    @Column(name = "phone_number", nullable = false, length = 30)
+    private String phoneNumber;
+    @Column(name = "city_of_living",nullable = false, length = 100)
+    private String cityOfLiving;
 }
